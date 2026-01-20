47a1823d4d8a63fc20d3d4d0d051c6aab2d69aa6
KaiIchiho <kaiichiho@example.com>
2026-01-20 10:55:27 +0900

    main.py

--- BackEnd/main.py
+++ BackEnd/main.py
@@ -4,4 +4,12 @@ app=FastAPI()
 
 @app.get("/api/hello")
 def root():
-    return {"Message" : "BackEnd Is Running!"}
+    a=step_one()
+    b=step_two()
+    return {"Message" : "BackEnd Is Running!", "a" : a, "b" : b}
+
+def step_one:
+    return "one"
+
+def step_two:
+    return "two"
