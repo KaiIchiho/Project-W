d373f381645d452dd1970e6731fe454e28f6a7b6
KaiIchiho <kaiichiho@example.com>
2026-04-02 14:43:24 +0900

    SVN

--- FrontEnd/room.js
+++ FrontEnd/room.js
@@ -126,8 +126,8 @@ function handleEnterRoom(data){
         if(data.user_is_player)as_player="Player";
         else as_player="Viewer";
         console.log("2");
-        console.log("Room ID: "+room_id+", As "+as_player);
-        room_info.textContent="Room ID: "+room_id+", As "+as_player;
+        console.log("Room ID: "+data.room_id+", As "+as_player);
+        room_info.textContent="Room ID: "+data.room_id+", As "+as_player;
         current_room_id=data.room_id;
         console.log("3");
         // console.log("Enter Room "+room_id+" Success");
