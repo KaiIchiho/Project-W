da953109836783f7766502a35a508305e5f76270
KaiIchiho <kaiichiho@example.com>
2026-04-06 16:59:09 +0900

    SVB

--- FrontEnd/WebSocket.js
+++ FrontEnd/WebSocket.js
@@ -85,6 +85,7 @@ function sendJson(data){
 }
 
 function handleWsMessage(event){
+    console.log("Method: handleWsMessage")
     console.log("type:", typeof event.data, event.data)
     const data=event.data
     if(data==null){
