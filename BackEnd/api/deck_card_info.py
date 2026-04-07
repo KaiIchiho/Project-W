421103e6b036f8dde78999f28cb11d82091018a5
KaiIchiho <kaiichiho@example.com>
2026-04-07 17:53:42 +0900

    SVN

--- BackEnd/api/deck_card_info.py
+++ BackEnd/api/deck_card_info.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
-from schemas.deck_card_info import DeckListRequest,DeckListResponse,SelectDeckResquest,SelectDeckResponse
+from schemas.deck_card_info import DeckListRequest,DeckListResponse
+# from schemas.game_flow import SelectDeckRequest,SelectDeckResponse
 from services import deck_card_info
 
 router=APIRouter()
@@ -8,7 +9,7 @@ router=APIRouter()
 def deck_list_endpoint(req:DeckListRequest):
     return deck_card_info.deck_list(req)
 
-@router.post("/select_deck",response_model=SelectDeckResponse)
-def select_deck_endpoint(req:SelectDeckResquest):
-    #return deck_card_info.deck_list(req)
-    pass
+# @router.post("/select_deck",response_model=SelectDeckResponse)
+# def select_deck_endpoint(req:SelectDeckRequest):
+#     #return deck_card_info.deck_list(req)
+#     pass
