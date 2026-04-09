c4579989991d5869da874b738e9e3a59f2191928
KaiIchiho <kaiichiho@example.com>
2026-04-09 18:06:03 +0900

    SVN

--- BackEnd/services/parse_model.py
+++ BackEnd/services/parse_model.py
@@ -1,6 +1,11 @@
 def parse_model(data: dict, model_cls):
+    # try:
+    #     return model_cls.model_validate(data)
+    # except Exception as e:
+    #     print("Model Parse Failed:", e)
+    #     return None
     try:
-        return model_cls.model_validate(data)
+        return model_cls.model_validate(data, mode='python')
     except Exception as e:
         print("Model Parse Failed:", e)
         return None
