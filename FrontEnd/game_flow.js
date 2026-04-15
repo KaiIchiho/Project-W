e605ccde91ec1e75a888d08102e9c6416d9fa3b7
KaiIchiho <kaiichiho@example.com>
2026-04-15 18:56:27 +0900

    SVN

--- FrontEnd/game_flow.js
+++ FrontEnd/game_flow.js
@@ -87,7 +87,7 @@ function handleClockPhaseDraw(data){
 function handledMainPhaseCharPlay(data){
     let common=data.common;
     updateHandByCommon(common);
-    updataStage(common.stage)
+    // updataStage(common.stage)
 }
 
 function nextPhase(){
