21eea352007fd8a489d34e7cfc579ac9da9cee82
KaiIchiho <kaiichiho@example.com>
2026-05-11 17:35:32 +0900

    SVN

--- BackEnd/services/game_flow.py
+++ BackEnd/services/game_flow.py
@@ -165,7 +165,7 @@ async def auto_set_first_player(game:Game):
     await game.send_data_to_room(res)
 
 async def end_game(game:Game,defeated_player_id:int):
-    if defeated_deck_id is not None:
+    if defeated_player_id is not None:
         winner_player_id=game.check_command_other_player(defeated_player_id)
         winner_identity=game.check_player_identity_by_id(winner_player_id)
         winned_deck_id,winned_deck_name=game.get_player_deck_info(winner_player_id)
